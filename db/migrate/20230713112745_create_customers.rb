6c1b935835c5a8f518caa3750f5a10d1518201ad
ShalaevPavel <lohgewt123@yandex.by>
2023-07-13 14:39:44 +0300

    added migrations for customer model.Execute rails db:migrate to attach migration

---
+++ db/migrate/20230713112745_create_customers.rb
@@ -0,0 +1,27 @@
+class CreateCustomers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :customers do |t|
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.datetime :created_at
+      t.datetime :updated_at
+      t.boolean :accepts_marketing
+      t.datetime :accepts_marketing_updated_at
+      t.string :tags
+      t.boolean :verified_email
+      t.string :state
+      t.integer :last_order_id
+      t.string :last_order_name
+      t.integer :orders_count
+      t.string :total_spent
+      t.boolean :tax_exempt
+      t.string :multipass_identifier
+      t.string :note
+      t.string :password
+      t.string :password_confirmation
+
+      t.timestamps
+    end
+  end
+end
