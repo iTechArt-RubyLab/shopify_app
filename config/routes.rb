e7349b77b17f210aa0657c09613ec29251236e1e
ikl0 <klimovs2008van@gmail.com>
2023-07-17 15:32:59 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root 'shopify_api#products'
   get '/login', to: 'shopify_auth#login'
   get '/auth/callback', to: 'shopify_auth#callback'
-  resources :orders, only: [:index]
+  resources :orders, only: [:index, :show, :create, :update] do
+    member do
+      put :cancel # Для отмены заказа
+    end
+  end
 end
