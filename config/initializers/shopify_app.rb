fab23fcb9ef23dbf772d62a72567334b7796f397
ikl0 <klimovs2008van@gmail.com>
2023-07-19 17:58:10 +0300

    add scopes

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,7 +2,7 @@ ShopifyAPI::Context.setup(
   api_key: ENV['SHOPIFY_API_KEY'],
   api_secret_key: ENV['SHOPIFY_API_SECRET'],
   host: ENV['HOST'],
-  scope: 'write_products,read_products',
+  scope: 'write_products,read_products,read_orders,write_orders',
   is_embedded: false,
   api_version: '2023-04',
   is_private: false
