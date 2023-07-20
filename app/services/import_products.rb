7f11f3e70e863d84b2e5ebb5d6098bb01dabb556
Ritamma <rita07041998@gmail.com>
2023-07-20 16:05:59 +0300

    Add import product

--- app/services/import_products.rb
+++ app/services/import_products.rb
@@ -10,7 +10,8 @@ module ImportProducts
 
   def create_products(shopify_products)
     shopify_products.each do |shopify_product|
-      Product.create(
+      ActiveRecord::Base.transaction do
+        product = Product.create!(
           body_html: shopify_product.body_html,
           created_at: shopify_product.created_at,
           handle: shopify_product.handle,
@@ -24,42 +25,37 @@ module ImportProducts
           title: shopify_product.title,
           updated_at: shopify_product.update_at,
           vendor: shopify_product.vendor
-      )
-      # ProductVariant.create(
-      #   barcode: shopify_product.barcode,
-      #   compare_at_price: shopify_product.variants.compare_at_price,
-      #   created_at: shopify_product.variants.created_at,
-      #   fulfillment_service: shopify_product.variants.fulfillment_service,
-      #   grams: shopify_product.variants.grams,
-      #   weight: shopify_product.variants.weight,
-      #   weight_unit: shopify_product.variants.weight_unit,
-      #   id: shopify_product.variants.id,
-      #   inventory_item_id: shopify_product.variants.inventory_item_id,
-      #   inventory_management: shopify_product.variants.inventory_management,
-      #   inventory_policy: shopify_product.variants.inventory_policy,
-      #   inventory_quantity: shopify_product.variants.inventory_quantity,
-      #   option1: shopify_product.variants.option1,
-      #   position: shopify_product.variants.position,
-      #   price: shopify_product.variants.price,
-      #   product_id: shopify_product.variants.product_id,
-      #   requires_shipping: shopify_product.variants.requires_shipping,
-      #   sku: shopify_product.variants.sku,
-      #   taxable: shopify_product.variants.taxable,
-      #   title: shopify_product.variants.title,
-      #   updated_at: shopify_product.variants.updated_at
-      #
-      #   )
-      # ProductOption.create (
-      #                        #     options: {
-      #                        #       id: shopify_product.options.id,
-      #                        #       product_id: shopify_product.options.product_id,
-      #                        #       name: shopify_product.options.name,
-      #                        #       position: shopify_product.options.position,
-      #                        #       values: [
-      #                        #         shopify_product.options.values
-      #                        #       ]
-      #                        #     },
-      #                      )
+        )
+        product.create_product_option(
+          id: shopify_product.options.first['id'],
+          product_id: shopify_product.options.first['product_id'],
+          name: shopify_product.options.first['name'],
+          position: shopify_product.options.first['position'],
+          )
+        product.create_product_variant(
+          barcode: shopify_product.barcode,
+          compare_at_price: shopify_product.compare_at_price,
+          created_at: shopify_product.created_at,
+          fulfillment_service: shopify_product.fulfillment_service,
+          grams: shopify_product.grams,
+          weight: shopify_product.weight,
+          weight_unit: shopify_product.weight_unit,
+          id: shopify_product.id,
+          inventory_item_id: shopify_product.inventory_item_id,
+          inventory_management: shopify_product.inventory_management,
+          inventory_policy: shopify_product.inventory_policy,
+          inventory_quantity: shopify_product.inventory_quantity,
+          option1: shopify_product.option1,
+          position: shopify_product.position,
+          price: shopify_product.price,
+          product_id: shopify_product.product_id,
+          requires_shipping: shopify_product.requires_shipping,
+          sku: shopify_product.sku,
+          taxable: shopify_product.taxable,
+          title: shopify_product.title,
+          updated_at: shopify_product.updated_at
+        )
+      end
     end
   end
 end
