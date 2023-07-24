7f642bd8819ee3cb0e2a09d34041a9b006ac7c27
Grechko Maria <grechko.maria2003@gmail.com>
2023-07-24 18:31:15 +0300

    Add ngrok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "localhost" << "4ac4-46-56-181-178.ngrok-free.app"
 end
