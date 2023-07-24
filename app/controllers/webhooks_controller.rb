aa7db4a1707610bb7c14c8aaffeb4f6d49dacdad
Grechko Maria <grechko.maria2003@gmail.com>
2023-07-24 18:29:39 +0300

    Add controller and route

---
+++ app/controllers/webhooks_controller.rb
@@ -0,0 +1,29 @@
+class WebhooksController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:product_updated]
+  
+  def product_updated
+    data = JSON.parse(request.body.read)
+    product_id = data['id']
+    
+    @product = Product.find_by(shopify_id: product_id)
+    if @product
+      assign_product_attributes(data)
+      if @product.save!
+        redirect_to @product, notice: 'Product was successfully updated.'
+      else
+        redirect_to @product, notice: 'Could not update the product.'
+      end
+    else
+      redirect_to products_path, alert: 'Could not find the product in local database.'
+    end
+  end
+  
+  private
+  
+  def assign_product_attributes(data)
+    @product.title = data['title']
+    @product.body_html = data['body_html']
+    @product.status = data['status']
+    @product.vendor = data['vendor']
+  end
+end
