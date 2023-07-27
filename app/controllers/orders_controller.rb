6767104045e7265f2977fa9123d60ac7627e4007
ikl0 <klimovs2008van@gmail.com>
2023-07-27 13:37:33 +0300

    changes for create and cancel

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,17 +12,21 @@ class OrdersController < ApplicationController
     @order = Order.new
   end
 
-  def edit; end
+  def edit
+    @line_items = @order.line_items
+  end
 
   def create
     @shopify_order = ShopifyAPI::Order.new(session: @session)
+    @local_order = Order.create(order_params)
+
     assign_order_attributes
-    @local_order = Order.new(order_params)
 
     if @shopify_order.save!
       @local_order.shopify_id = @shopify_order.id
       @local_order.name = @shopify_order.name
-      #TODO :  @local_order.line_items = @shopify_order.line_items
+      @local_order.total_price = @shopify_order.total_price
+
       @local_order.save!
       redirect_to orders_path, notice: 'Order was successfully created.'
     else
@@ -33,13 +37,6 @@ class OrdersController < ApplicationController
   def update
     @shopify_order = ShopifyAPI::Order.find(session: @session, id: @order.shopify_id)
     assign_order_attributes
-    @shopify_order.transactions = [
-      {
-        "kind" => "sale",
-        "status" => "success",
-        "amount" => 111.22
-      }
-    ]
     if @shopify_order.save! && @order.update(order_params)
       redirect_to @order, notice: 'Order was successfully updated.'
     else
@@ -51,6 +48,7 @@ class OrdersController < ApplicationController
   def cancel
     @shopify_order = ShopifyAPI::Order.find(session: @session, id: @order.shopify_id)
     if @shopify_order.cancel
+      CanceledOrder.create(order: @order)
       redirect_to @order, notice: 'Заказ успешно отменен.'
     else
       redirect_to @order, alert: 'Ошибка при отмене заказа.'
@@ -85,45 +83,14 @@ class OrdersController < ApplicationController
         title: Product.find_by(id: product_id).title,
         product_id: product_id,
         quantity: product_quantities[index],
-        order_id: @shopify_order.id,
+        order_id: @local_order.id,
         grams: "1300",
-        price: 50.00,
+        price: 51.00,
 
       )
-      line_item.save
-
-
+      line_item.save!
       @shopify_order.line_items << line_item
-      end
-=begin
-      @shopify_order.transactions = [
-        {
-          "kind" => "sale",
-          "status" => "success",
-          "amount" => 238.47
-        }
-      ]
-      @shopify_order.total_tax = 13.5
-      @shopify_order.currency = "EUR"
-      end
-=end
-=begin
-    @shopify_order.line_items = [
-      {
-        "title" => "Big Brown Bear Boots",
-        "price" => 60.00,
-        "grams" => "1300",
-        "quantity" => 3,
-        "tax_lines" => [
-          {
-            "price" => 13.5,
-            "rate" => 0.06,
-            "title" => "State tax"
-          }
-        ]
-      }
-    ]
-=end
+    end
   end
 
   def create_session
