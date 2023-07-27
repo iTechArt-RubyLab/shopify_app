6fcacd987063aced3aec3f329e056c8d7901a9c3
ikl0 <klimovs2008van@gmail.com>
2023-07-27 13:37:55 +0300

    add route for cancel

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root 'shopify_api#products'
   get '/login', to: 'shopify_auth#login'
   get '/auth/callback', to: 'shopify_auth#callback'
-  resources :orders
+  resources :orders do
+    member do
+      post :cancel
+    end
+  end
   resources :products
 end
