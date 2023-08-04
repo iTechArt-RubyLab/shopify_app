9d55822b302c0a5785a60269a507f84606ec7d5e
ikl0 <klimovs2008van@gmail.com>
2023-08-04 10:06:07 +0300

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -203,6 +203,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_01_134621) do
     t.string "vendor"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "admin_graphql_api_id"
   end
 
   create_table "properties", force: :cascade do |t|
