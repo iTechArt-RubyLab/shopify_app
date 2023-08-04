d1a6005d511775a4a843b686fd3a9373ee95028a
ikl0 <klimovs2008van@gmail.com>
2023-08-04 09:35:22 +0300

    add logic for edit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,34 +13,72 @@ class OrdersController < ApplicationController
   end
 
   def edit
-    @line_items = @order.line_items
+    @selected_product_titles = @order.line_items.pluck(:title)
+    @selected_product_quantities = @order.line_items.pluck(:quantity)
   end
 
   def create
     @shopify_order = ShopifyAPI::Order.new(session: @session)
-    @local_order = Order.create(order_params)
+    @order = Order.new(order_params)
 
     assign_order_attributes
 
     if @shopify_order.save!
-      @local_order.shopify_id = @shopify_order.id
-      @local_order.name = @shopify_order.name
-      @local_order.total_price = @shopify_order.total_price
+      @order.shopify_id = @shopify_order.id
+      @order.name = @shopify_order.name
+      @order.total_price = @shopify_order.total_price
 
-      @local_order.save!
-      redirect_to orders_path, notice: 'Order was successfully created.'
+      @shopify_order.line_items.each do  |line_item|
+
+        params = line_item.except("id", "admin_graphql_api_id","properties", "tax_lines", "price_set", "duties", "discount_allocations", "total_discount_set" )
+        params["shopify_id"] = line_item["id"]
+
+
+        order_line_item =  @order.line_items.new(params)
+        order_line_item.price_set = PriceSet.new(line_item["price_set"])
+        #order_line_item.duties.new(line_item["duties"])
+        #order_line_item.discount_allocations.new(line_item["discount_allocations"])
+        #order_line_item.total_discount_set.new(line_item["total_discount_set"])
+
+        order_line_item.save
+      end
+      @order.save!
+      redirect_to orders_path
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def update
-    @shopify_order = ShopifyAPI::Order.find(session: @session, id: @order.shopify_id)
-    assign_order_attributes
-    if @shopify_order.save! && @order.update(order_params)
-      redirect_to @order, notice: 'Order was successfully updated.'
+    @shopify_order = ShopifyAPI::Order.new(session: @session)
+    @shopify_order.id = @order.shopify_id
+
+    #assign_order_attributes
+    @shopify_order.line_items = []
+    if @shopify_order.save!
+      @order.shopify_id = @shopify_order.id
+      @order.name = @shopify_order.name
+      @order.total_price = @shopify_order.total_price
+
+      @shopify_order.line_items.each do  |line_item|
+
+        params = line_item.except("id", "admin_graphql_api_id","properties", "tax_lines", "price_set", "duties", "discount_allocations", "total_discount_set" )
+        params["shopify_id"] = line_item["id"]
+
+
+        order_line_item =  @order.line_items.new(params)
+        order_line_item.price_set = PriceSet.new(line_item["price_set"])
+        #order_line_item.duties.new(line_item["duties"])
+        #order_line_item.discount_allocations.new(line_item["discount_allocations"])
+        #order_line_item.total_discount_set.new(line_item["total_discount_set"])
+
+        order_line_item.save
+      end
+
+      @order.save!
+      redirect_to orders_path
     else
-      render :edit
+      redirect_to orders_path, alert: 'Error while editing the order'
     end
   end
 
@@ -49,9 +87,9 @@ class OrdersController < ApplicationController
     @shopify_order = ShopifyAPI::Order.find(session: @session, id: @order.shopify_id)
     if @shopify_order.cancel
       CanceledOrder.create(order: @order)
-      redirect_to @order, notice: 'Заказ успешно отменен.'
+      redirect_to orders_path
     else
-      redirect_to @order, alert: 'Ошибка при отмене заказа.'
+      redirect_to @order, alert: 'Error while canceling the order'
     end
   end
 
@@ -78,22 +116,49 @@ class OrdersController < ApplicationController
     product_ids = params[:order][:product_ids].reject(&:empty?)
     product_quantities = params[:order][:product_quantities].map(&:to_i)
     @shopify_order.line_items = []
-    product_ids.each_with_index do |product_id, index|
-      line_item = LineItem.new(
-        title: Product.find_by(id: product_id).title,
-        product_id: product_id,
-        quantity: product_quantities[index],
-        order_id: @local_order.id,
-        grams: "1300",
-        price: 51.00,
 
-      )
-      line_item.save!
-      @shopify_order.line_items << line_item
+    product_ids.each_with_index do |product_id, index|
+      @shopify_order.line_items << {  title: Product.find_by(id: product_id).title,
+                                      product_id: Product.find_by(id: product_id).shopify_id,
+                                      quantity: product_quantities[index],
+                                      grams: "1300",
+                                      price: 51.00}
     end
   end
+
+  def change_order_attributes
+    product_ids = params[:order][:product_ids].reject(&:empty?)
+    product_quantities = params[:order][:product_quantities].map(&:to_i)
+
+    product_ids.each_with_index do |product_id, index|
+
+      line_item = @shopify_order.line_items.detect { |item| item["id"] == Product.find_by(id: product_id).shopify_id }
+
+      # line_item = @shopify_order.line_items.find_by(product_id: Product.find_by(id: product_id).shopify_id)
+
+      if line_item.present?
+        if params[:order][:product_ids].include?(product_id.to_s)
+          line_item.update(
+            quantity: product_quantities[index],
+          )
+        else
+          line_item.destroy
+        end
+      else
+        @shopify_order.line_items << {  title: Product.find_by(id: product_id).title,
+                                       product_id: Product.find_by(id: product_id).shopify_id,
+                                       quantity: product_quantities[index],
+                                       grams: "1300",
+                                       price: 51.00}
+      end
+    end
+  end
+
+
 
   def create_session
     @session = ShopifyAPI::Auth::Session.new(shop: ENV['SHOP'], access_token: cookies[:shopify_app_session])
   end
+
 end
+
