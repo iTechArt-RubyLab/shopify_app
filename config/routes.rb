66f9a57d646e0cbc9b68a409668475b3f517cdcf
ikl0 <klimovs2008van@gmail.com>
2023-08-08 19:50:42 +0300

    add route for update

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/import_shopify_data', to: 'shopify_import#import'
   post '/webhooks/shopify/product_updated', to: 'webhooks#product_updated'
   post '/webhooks/shopify/order_cancelled', to: 'webhooks#order_cancelled'
+  post '/webhooks/shopify/order_updated', to: 'webhooks#order_updated'
   resources :orders do
     member do
       post :cancel
